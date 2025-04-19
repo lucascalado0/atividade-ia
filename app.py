20d75b34771eaf95973cd04d22e90b6a0ac16c0c
devlucascalado <devlucascalado@gmail.com>
2025-04-19 19:00:26 -0300

    projeto final

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, render_template
 import joblib
 import numpy as np
 
@@ -6,7 +6,7 @@ import numpy as np
 app = Flask(__name__)
 
 # Carrega o modelo
-modelo = joblib.load(r'C:\Users\Lucas Vinicius\Desktop\IA\modelo_mlp.pkl')
+modelo = joblib.load(r'C:\Users\Lucas Vinicius\Desktop\IA\modelo_rg.pkl')
 
 
 # Rota padrão
@@ -16,7 +16,7 @@ def home():
 
 app = Flask(__name__)
 
-@app.route('/teste', methods=["GET", "POST"])
+@app.route('/home', methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         try:
@@ -25,16 +25,9 @@ def index():
             texture_mean = float(request.form.get("texture"))
             perimeter_mean = float(request.form.get("perimeter"))
             area_mean = float(request.form.get("area"))
-            smoothness_mean = float(request.form.get("smoothness"))
-            compactness_mean = float(request.form.get("compactness"))
-            concavity_mean = float(request.form.get("concavity"))
-            concave_mean = float(request.form.get("concave"))
-            symmetry_mean = float(request.form.get("symmetry"))
-            fractal_dimension_mean = float(request.form.get("fractal"))
 
             # Agrupa os dados em um array para predição
-            features = np.array([[radius_mean, texture_mean, perimeter_mean, area_mean, smoothness_mean,
-                                  compactness_mean, concavity_mean, concave_mean, symmetry_mean, fractal_dimension_mean]])
+            features = np.array([[radius_mean, texture_mean, perimeter_mean, area_mean]])
             
             # Realiza a predição
             previsao = modelo.predict(features)
@@ -49,22 +42,5 @@ def index():
     return render_template("index.html")
 
 
- #Rota para fazer previsão
-# @app.route('/predict', methods=["POST"])
-# def predict():
-#     dados = request.get_json()
-#     if not dados:
-#             return jsonify({'erro': 'Nenhum dado recebido'}), 400
-
-#     try:
-#         # Espera uma lista de 30 features como input
-#             entrada = np.array(dados['features']).reshape(1, -1)
-#             predicao = modelo.predict(entrada)
-#             resultado = 'Maligno' if predicao[0] == 1 else 'Benigno'
-#             return jsonify({'resultado': resultado})
-#     except Exception as e:
-#         return jsonify({'erro': str(e)}), 500
-
-# Executa o app
 if __name__ == '__main__':
     app.run(debug=True)
